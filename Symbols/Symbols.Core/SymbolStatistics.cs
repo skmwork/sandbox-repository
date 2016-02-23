f534ad526ba56e69a0ac60cb3602b474ac1b64ba
Konstantin Sergeev <skmwork@yandex.ru>
2016-02-23 16:36:54 +0300

    Добавил логику подсчета символов

---
+++ Symbols/Symbols.Core/SymbolStatistics.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Symbols.Core
+{
+    public class SymbolStatistics
+    {
+        public Dictionary<char, int> Statistics { get; private set; }
+
+        public SymbolStatistics()
+        {
+            Statistics = new Dictionary<char, int>();
+        }
+
+        public void AddStatistic(string input)
+        {         
+            input.ToCharArray().ToList().ForEach(x =>
+            {
+                if (!Statistics.ContainsKey(x))
+                {
+                    Statistics.Add(x, 1);
+                    return;
+                }
+                Statistics[x] += 1;
+            });
+        }
+
+        public Dictionary<char, int> Top5Symbols
+        {
+            get { return Statistics.OrderByDescending(x => x.Value).ThenBy(v => v.Key).Take(5).ToDictionary(x => x.Key, v => v.Value); }
+        }
+
+    }
+}

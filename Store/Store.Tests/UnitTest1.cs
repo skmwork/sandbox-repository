743544401403a5d3ee44a657974906998c978641
Konstantin Sergeev <skmwork@yandex.ru>
2016-07-08 18:48:19 +0300

    Добавил пейджинг

---
+++ Store/Store.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Store.Domain.Abstract;
+using Store.Domain.Entities;
+using Store.WebUI.Controllers;
+
+namespace Store.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+new Product {ProductId = 1, Name = "P1"},
+new Product {ProductId = 2, Name = "P2"},
+new Product {ProductId = 3, Name = "P3"},
+new Product {ProductId = 4, Name = "P4"},
+new Product {ProductId = 5, Name = "P5"}
+}.AsQueryable());
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+    }
+}
